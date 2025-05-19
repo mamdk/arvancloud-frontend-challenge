2f93d5d95733e5d952355863c1e5c01c8cd7046d
mhmk <mhm1379mhm3@gmail.com>
2025-05-19 23:39:33 +0330

    fix: add Form

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { useAuth } from 'src/contexts/auth';
 import styles from './index.module.sass';
+import Form from 'src/components/section/form';
+import Field from 'src/components/ui/field';
+import Input from 'src/components/ui/input';
 
 function LoginPage() {
 	const auth = useAuth();
@@ -20,11 +23,23 @@ function LoginPage() {
 
 	return (
 		<main className={styles.loginPage}>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
+			<Form
+				title={'Sign-up'}
+				action={{
+					title: 'sign-up',
+					handler: () => {
+						console.log('click');
+					},
+					fullWidth: true,
 				}}
-			></form>
+			>
+				<Field label={'Email'} fullWidth>
+					<Input name={'email'} placeholder={'Email'} fullWidth />
+				</Field>
+				<Field label={'Password'} fullWidth>
+					<Input type={'password'} name={'password'} placeholder={'Password'} fullWidth />
+				</Field>
+			</Form>
 		</main>
 	);
 }
