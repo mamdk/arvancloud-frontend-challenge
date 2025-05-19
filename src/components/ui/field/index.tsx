142eb17a53a450583d8a934f40b5cf2b7601c49f
mhmk <mhm1379mhm3@gmail.com>
2025-05-19 17:09:23 +0330

    feat: filed

---
+++ src/components/ui/field/index.tsx
@@ -0,0 +1,30 @@
+import styles from './index.module.sass';
+import cls from 'src/utils/class_names';
+import { type ReactElement } from 'react';
+
+interface TFieldProps {
+	className?: string;
+	error?: boolean;
+	required?: boolean;
+	message?: string;
+	label?: string;
+	children?: any;
+	fullWidth?: boolean;
+}
+
+export function Field({ children, className, error = false, message, label, required, fullWidth }: TFieldProps) {
+	return (
+		<div className={cls(styles.fieldSection, className, fullWidth && styles.fullWidth)}>
+			{label && (
+				<label className={cls(styles.label)}>
+					{label}
+					{required && <span>*</span>}
+				</label>
+			)}
+			{children || <div className={cls(styles.replaceMe)}>Replace me</div>}
+			{message && <p className={cls(styles.message, error && styles.error)}>{message}</p>}
+		</div>
+	);
+}
+
+export default Field;
